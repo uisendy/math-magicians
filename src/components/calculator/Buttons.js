60f33043fb905a148bf6d4aa7f1081babac2057b
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-09 15:38:34 +0100

    [calc-func]: Update: Pass props,

--- src/components/calculator/Buttons.js
+++ src/components/calculator/Buttons.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Button from './Button';
 
-const Buttons = ({ operandsAndOperators }) => (
-  <div className="calc-keys">
-    {operandsAndOperators.map((calcKeys, index) => (
-      <Button value={calcKeys} id={index} key={calcKeys} />
-    ))}
+const Buttons = ({ operandsAndOperators, handleClick }) => (
+  <div className="button__container">
+    <div className="calc-keys">
+      {operandsAndOperators.map((calcKeys, index) => (
+        <Button
+          value={calcKeys}
+          id={index}
+          key={calcKeys}
+          handleClick={handleClick}
+        />
+      ))}
+    </div>
   </div>
 );
 
