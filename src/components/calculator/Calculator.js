9876f075f00d87ae8c2ce9fe53c1fc5ba5966ded
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-10 07:11:45 +0100

    [calc-func]: refactoring: Change class component to functional component

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,34 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Buttons from './Buttons';
 import calculate from '../../logic/calculate';
 import './calculator.css';
 import calcBtn from '../../logic/calculatorButtons';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.obj = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-    this.state = this.obj;
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (e) => {
-    this.setState((state) => calculate(state, e.target.textContent));
-  };
-
-  render() {
-    return (
-      <div className="main__container">
-        <div className="calc__container">
-          <Display display={this.state} />
-          <Buttons calcBtn={calcBtn} handleClick={this.handleClick} />
-        </div>
+const Calculator = () => {
+  return (
+    <div className="main__container">
+      <div className="calc__container">
+        <Display displayObj={displayObj} />
+        <Buttons calcBtn={calcBtn} handleClick={handleClick} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Calculator;
