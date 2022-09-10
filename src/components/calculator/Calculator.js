5abc0c08cf53faf1c1211fb85ade1a5df767632e
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-10 07:12:28 +0100

    [calc-func]: refactoring: removed Class state properties and apply the use fof UseState Hooks

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -6,6 +6,16 @@ import './calculator.css';
 import calcBtn from '../../logic/calculatorButtons';
 
 const Calculator = () => {
+  const [displayObj, setDisplayObj] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
+
+  const handleClick = (e) => {
+    setDisplayObj(() => calculate(displayObj, e.target.textContent));
+  };
+
   return (
     <div className="main__container">
       <div className="calc__container">
