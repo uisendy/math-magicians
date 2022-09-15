8a8f6c2af06740d4f11f51c689160e95bbb686fb
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-15 16:16:08 +0100

    [feature-route]: Add Header.js Component and Setup route

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ({ title }) => (
+  <div>
+    <h2 style={{ fontSize: '2rem', padding: '1.5rem 0' }}>{title}</h2>
+  </div>
+);
+
+export default Header;
