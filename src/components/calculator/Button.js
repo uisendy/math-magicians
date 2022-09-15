86a99904527dfd3f7b5131d329cac67c10356b13
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-15 16:17:16 +0100

    [feature-route]: Update: Solve bug Button.JS to fix Linters error

--- src/components/calculator/Button.js
+++ src/components/calculator/Button.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Button = ({ value, id, handleClick }) => (
   <button id={id} className="calc__btn" type="button" onClick={handleClick}>
     {value}
   </button>
 );
-
-Button.propTypes = {
-  value: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired,
-};
 
 export default Button;
