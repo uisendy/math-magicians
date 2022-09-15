c259bfaac2f0eab77be7c86800b9755ac60b76f3
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-15 16:15:09 +0100

    [refactor-hooks]: Setup: Wrappimg of Router, router and BrowserRoute

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import CalculatorPage from './components/pages/CalculatorPage';
+import Quotes from './components/pages/Quotes';
+import NotMatch from './components/pages/NotMatch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/calculator" element={<CalculatorPage />} />
+      <Route path="/quote" element={<Quotes />} />
+
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
+  </BrowserRouter>,
 );
