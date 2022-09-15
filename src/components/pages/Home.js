a946499c2e82a431953699668edf72638b617cf3
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-15 16:18:42 +0100

    [feature-route]: Add Home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../Header';
+
+const Home = () => (
+  <div style={{ padding: '2rem 6.5%' }}>
+    <Header title="Welcome to Our page" />
+    <p style={{ fontSize: '1.5rem' }}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nulla, cum
+      doloremque, reiciendis at excepturi rerum aut totam vitae, odio laudantium
+      natus error ratione architecto dicta dolor aperiam nesciunt et vero animi
+      esse! Fugiat suscipit provident dolorum aliquam excepturi pariatur sed
+      laboriosam vero placeat ex. Qui eligendi facere ab suscipit.
+      <br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit,
+      assumenda fugiat magni ipsa optio vel dolorem maiores maxime earum
+      similique ratione porro consectetur expedita accusantium iure qui
+      consequuntur dolor iste.
+    </p>
+  </div>
+);
+
+export default Home;
